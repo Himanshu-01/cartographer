91101002f18afac59fbe30dfbb731f7fc1f385d3
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-08-18 10:44:28 -0400

    rewrite singleplayer pause menu code
    - adds required logic for simulation events to the pause menu interface

--- xlive/H2MOD.cpp
+++ xlive/H2MOD.cpp
@@ -37,8 +37,8 @@
 #include "interface/first_person_weapons.h"
 #include "interface/new_hud.h"
 #include "interface/new_hud_draw.h"
-#include "interface/user_interface.h"
 #include "interface/user_interface_controller.h"
+#include "interface/user_interface_objectives.h"
 #include "interface/user_interface_text.h"
 #include "interface/user_interface_utilities.h"
 #include "interface/screens/screens_patches.h"
@@ -98,7 +98,6 @@
 #ifdef TERMINAL_ENABLED
 #include "H2MOD/GUI/ImGui_Integration/Console/CommandCollection.h"
 #endif
-#include "H2MOD/Modules/Accounts/AccountLogin.h"
 #include "H2MOD/Modules/CustomVariantSettings/CustomVariantSettings.h"
 #include "H2MOD/Modules/DirectorHooks/DirectorHooks.h"
 #include "H2MOD/Modules/EventHandler/EventHandler.hpp"
@@ -648,6 +647,7 @@ static void h2mod_apply_hooks(void)
 		unit_apply_patches();
 
 		user_interface_text_apply_hooks();
+		user_interface_objectives_apply_patches();
 		user_interface_controller_apply_patches();
 		hud_messaging_apply_hooks();
 		font_group_apply_hooks();
